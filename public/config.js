15567aa28cada359e1694455a9b115be9cf7abfb
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-09-20 08:24:53 +0530

    Make app name and description configurable

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 window.config = {
-    testConfig: 'My custom config 2',
+    appName: 'App name',
+    appDescription: 'App desc',
     itemsEndpoint: '/rbln/dummy-item-service/api-e04/1.0.0/items',
     apiPrefix: '/api',
     backgroundColor: 'rgba(218, 232, 252, 0.825)'
