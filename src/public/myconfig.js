66d1a3905fff203683d0ffbdcf7f3038a0cdd2fe
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-10-24 15:54:28 +0530

    Add default config

---
+++ src/public/myconfig.js
@@ -0,0 +1,7 @@
+export const myconfig = {
+    appName: 'My App 1',
+    appDescription: 'App desc',
+    itemsEndpoint: '/fftm/item-service/api-e04/v1.0/items',
+    apiPrefix: '/choreo-apis',
+    backgroundColor: 'rgba(218, 232, 252, 0.825)'
+};
