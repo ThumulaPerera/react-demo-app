15bfcc3122a6b201fad35b06db3eeff8cee4755b
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-10-31 16:11:50 +0530

    update api url

--- src/public/myconfig.js
+++ src/public/myconfig.js
@@ -1,7 +1,7 @@
 export const myconfig = {
     appName: 'My App 1',
     appDescription: 'App desc',
-    itemsEndpoint: '/glke/react-app/api-e04/v1.0/items',
+    itemsEndpoint: '/fftm/item-service/api-e04/v1.0/items',
     apiPrefix: '/choreo-apis',
     backgroundColor: 'rgba(218, 232, 252, 0.825)'
 };
