f52afab7c6e72deacbc29d5ef1447560dd8a9560
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-11-02 19:59:54 +0530

    Remove unnecessary file

--- public/config.js
+++
@@ -1,6 +0,0 @@
-window.config = {
-    appName: 'Managed Authentication',
-    itemsEndpoint: '/lshv/itemservice/api-e04/v1.0/items',
-    apiPrefix: '/choreo-apis',
-    backgroundColor: 'rgba(255, 255, 255, 1)'
-};
